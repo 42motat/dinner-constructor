88dee916b4335980b5fcb5da39d5e3f4d154fced
42motat <maxotatarnikov@yandex.ru>
2025-05-01 15:34:39 +0800

    реализована логика работы программы согласно требований ТЗ. код подготовлен для отправки на код-ревью

--- src/ru/practicum/dinner/DinnerConstructor.java
+++ src/ru/practicum/dinner/DinnerConstructor.java
@@ -1,5 +1,71 @@
 package ru.practicum.dinner;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Random;
+
 public class DinnerConstructor {
+    Random random = new Random();
+
+    // меню (список всех типов блюд и самих блюд)
+    HashMap<String, ArrayList<String>> menu = new HashMap<>();
+    ArrayList<String> dishes = new ArrayList<>();
+
+    // переменная служит для запоминания пользовательского ввода в конструкторе комбинаций блюд
+    ArrayList<String> swapForCombo = new ArrayList<>();
+
+    /* Логика добавления отдельного метода checkType():
+     * метод возвращает булево значение, которое можно использовать для других методов,
+     * чтобы не прописывать код проверки наличия типа блюд в каждом отдельном методе.
+     */
+    public boolean checkType(String dishType) {
+        return menu.containsKey(dishType);
+    }
+
+    /* Метод для добавления новых типов и новых блюд;
+     * нет проверки добавления в список уже существующих блюд, поскольку в ТЗ нет чёткого указания,
+     * что блюда в своих типах должны быть уникальными / не должно быть повторяющихся блюд в разных типах;
+     * в связи с этим реализована только проверка существования типов блюд.
+     */
+    public void mapDish(String dishType, String dishName) {
+        if (!(checkType(dishType))) {
+            dishes = new ArrayList<>();
+        }
+        dishes.add(dishName);
+        menu.put(dishType, dishes);
+        // информационная строка для подтверждения, что блюдо добавлено.
+        System.out.println("блюдо " + dishName + " добавлено к типу блюд " + dishType);
+    }
+
+    /* Основной метод генерации комбинаций блюд из указываемых пользователем типов блюд.*/
+    public void constructCombos(int numberOfCombos) {
+
+        ArrayList<ArrayList<String>> combos = new ArrayList<>();
+        for (int i = 0; i < numberOfCombos; i++) {
+            ArrayList<String> combo = new ArrayList<>();
+            for (String menuKey : swapForCombo) {
+                dishes = menu.get(menuKey);
+                int index = random.nextInt(dishes.size());
+                String dish = dishes.get(index);
+                combo.add(dish);
+            }
+            combos.add(combo);
+        }
+        for (int i = 0; i < combos.size(); i++) {
+            System.out.println("Комбо " + (i + 1));
+            System.out.println(combos.get(i));
+
+        }
+    }
+
+    /* Дополнительный метод для вывода всех существующих на данный момент типов блюд и названий блюд.*/
+    public void printMenu() {
+        for (String type : menu.keySet()) {
+            System.out.println("тип блюда " + type + " содержит:");
+            for (String dish : menu.get(type)) {
+                System.out.println("    " + dish);
+            }
+        }
+    }
 
 }
--- src/ru/practicum/dinner/Main.java
+++ src/ru/practicum/dinner/Main.java
@@ -1,5 +1,7 @@
 package ru.practicum.dinner;
 
+import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -24,7 +26,14 @@ public class Main {
                     break;
                 case "3":
                     return;
+                // не указано в ТЗ, добавлено для удобства тестирования
+                case "0":
+                    dc.printMenu();
+                    break;
             }
+
+            System.out.printf("%n%s", "=".repeat(42));
+            System.out.println();
         }
     }
 
@@ -33,6 +42,8 @@ public class Main {
         System.out.println("1 - Добавить новое блюдо");
         System.out.println("2 - Сгенерировать комбинации блюд");
         System.out.println("3 - Выход");
+        // дополнительные опции
+        System.out.println("0 - Вывод существующих категорий и их содержание");
     }
 
     private static void addNewDish() {
@@ -42,6 +53,7 @@ public class Main {
         String dishName = scanner.nextLine();
 
         // добавьте новое блюдо
+        dc.mapDish(dishType, dishName);
     }
 
     private static void generateDishCombo() {
@@ -51,15 +63,27 @@ public class Main {
         int numberOfCombos = scanner.nextInt();
         scanner.nextLine();
 
-        System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). Для завершения ввода введите пустую строку");
+        System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). " +
+                           "Для завершения ввода введите пустую строку");
         String nextItem = scanner.nextLine();
 
         //реализуйте ввод типов блюд
         while (!nextItem.isEmpty()) {
-
+            if (dc.checkType(nextItem)) {
+                dc.swapForCombo.add(nextItem);
+            } else {
+                System.out.println("В списке доступных типов блюд нет типа " + nextItem);
+                System.out.println("Вводите типы блюда, разделяя символом переноса строки (enter). " +
+                                   "Для завершения ввода введите пустую строку");
+            }
+            nextItem = scanner.nextLine();
         }
 
         // сгенерируйте комбинации блюд и выведите на экран
-
+        dc.constructCombos(numberOfCombos);
+        /* После генерации комбинаций блюд обнуляется список ранее введённых блюд;
+         * в противном случае, при повторном запуске команды "2" новые комбинации добавляются к уже существующим.
+         */
+        dc.swapForCombo = new ArrayList<>();
     }
 }
